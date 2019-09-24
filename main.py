f3a32876d70242c0795302c0b6ef036094f5b687
Konstantin Malanchev <hombit@gmail.com>
2019-09-24 22:26:47 +0300

    Add search in cone web page

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-import logging
 import re
+import urllib.parse
+
+from astropy.coordinates.name_resolve import get_icrs_coordinates, NameResolveError
 
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
 from app import app
+from search import get_layout as get_search_layout
 from viewer import get_layout as get_viewer_layout
 
 
@@ -30,6 +33,33 @@ app.layout = html.Div([
             ),
         ]
     ),
+    html.Div(
+        [
+            dcc.Input(
+                id='input-coord-or-name',
+                placeholder='Coordinates or object name',
+                type='text',
+                minLength=1,
+                maxLength=50,
+                n_submit=0,
+            ),
+            dcc.Input(
+                value='1',
+                id='input-search-radius',
+                placeholder='radius, arcsec',
+                type='number',
+                step='0.1',
+                min='0',
+                max='60',
+                n_submit=0,
+            ),
+            html.Button(
+                'Go',
+                id='button-coord-or-name',
+                n_clicks=0,
+            ),
+        ]
+    ),
     html.Div(id='page-content'),
 ])
 
@@ -39,16 +69,28 @@ app.layout = html.Div([
     [
         Input('button-oid', 'n_clicks'),
         Input('input-oid', 'n_submit'),
+        Input('button-coord-or-name', 'n_clicks'),
+        Input('input-coord-or-name', 'n_submit'),
+        Input('input-search-radius', 'n_submit'),
     ],
     state=[
         State('input-oid', 'value'),
+        State('input-coord-or-name', 'value'),
+        State('input-search-radius', 'value'),
         State('url', 'pathanme'),
     ]
 )
-def go_to_oid_view(n_clicks, n_submit, oid, current_pathaname):
-    if (n_submit == 0 and n_clicks == 0) or oid is None:
-        return current_pathaname
-    return f'/view/{oid}'
+def go_to_url(n_clicks_oid, n_submit_oid, n_clicks_search,
+              n_submit_coord_or_name, n_submit_radius,
+              oid,
+              coord_or_name, radius_arcsec,
+              current_pathaname):
+    if (n_submit_oid != 0 or n_clicks_oid != 0) and oid is not None:
+        return f'/view/{oid}'
+    if n_clicks_search != 0 or n_submit_coord_or_name != 0 or n_submit_radius != 0:
+        coord_or_name = urllib.parse.quote(coord_or_name)
+        return f'/search/{coord_or_name}/{radius_arcsec}'
+    return current_pathaname
 
 
 @app.callback(
@@ -56,10 +98,33 @@ def go_to_oid_view(n_clicks, n_submit, oid, current_pathaname):
     [Input('url', 'pathname')]
 )
 def app_select_by_url(pathname):
-    if re.search(r'/+$', pathname):
+    if re.search(r'^/+$', pathname):
         return html.Div()
-    if re.search(r'/+view/+\d{15}/*', pathname):
+    if re.search(r'^/+view/+\d{15}/*$', pathname):
         return get_viewer_layout(pathname)
+    search_match = re.search(r'^/+search/(?P<coord_or_name>[^/]+)/(?P<radius_arcsec>[^/]+)/*$', pathname)
+    if search_match:
+        try:
+            coord_or_name = urllib.parse.unquote(search_match['coord_or_name'])
+            coordinates = get_icrs_coordinates(coord_or_name)
+        except NameResolveError:
+            return html.Div(
+                [
+                    html.H1('404'),
+                    html.P('Cannot find such coordinate or name'),
+                ]
+            )
+        try:
+            radius_arcsec = float(search_match['radius_arcsec'])
+        except ValueError:
+            return html.Div(
+                [
+                    html.H1('404'),
+                    html.P('Wrong radius format'),
+                ]
+            )
+
+        return get_search_layout(coordinates, radius_arcsec)
     return html.H1('404')
 
 
---
+++ search.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+
+import dash_html_components as html
+import dash_dangerously_set_inner_html as ddsih
+from astropy.table import Table
+
+from cross import find_ztf_circle
+from util import html_from_astropy_table
+
+
+COLUMNS = {
+    'oid': 'OID',
+    'separation': 'separation, arcsec',
+    'filter': 'filter',
+    'ngoodobs': 'Number of "good" observations',
+    'duration': 'Duration, days',
+}
+
+
+@lru_cache(maxsize=128)
+def get_layout(coordinates, radius_arcsec):
+    ra = coordinates.ra.to_value('deg')
+    dec = coordinates.dec.to_value('deg')
+    j = find_ztf_circle.find(ra, dec, radius_arcsec)
+    if not j:
+        return html.Div('404')
+    table = Table([dict(oid=f'<a href="/view/{oid}">{oid}</a>', separation=obj['separation'], **obj['meta'])
+                   for oid, obj in j.items()])
+    layout = html.Div(
+        [
+            html.H1(f'Objects inside cone ({ra:.5f} deg, {dec:.5f} deg), r = {radius_arcsec:.1f}″'),
+            ddsih.DangerouslySetInnerHTML(html_from_astropy_table(table, COLUMNS)),
+        ],
+    )
+    return layout
