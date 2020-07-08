cf081917da51a0321f808cdabdcdb5f117cffa6b
Konstantin Malanchev <hombit@gmail.com>
2020-07-08 17:27:56 +0300

    Panstarrs in Aladin
    Fix #10

--- static/js/aladin_helper.js
+++ static/js/aladin_helper.js
@@ -5,7 +5,7 @@ var aladin_dec = parseFloat(document.getElementById('aladin-dec').innerText);
 
 var aladin = A.aladin(
     '#aladin-lite-div',
-    {survey: "P/DSS/color", fov: 2.0 / 60.0, target: aladin_coord}
+    {survey: 'P/PanSTARRS/DR1/color/z/zg/g', fov: 2.0 / 60.0, target: aladin_coord}
 );
 var marker = A.marker(
     aladin_ra,
