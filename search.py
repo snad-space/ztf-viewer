fd0cca2fc2605415ed7ee084aea3a5a6fce38f8a
Konstantin Malanchev <hombit@gmail.com>
2020-07-15 13:49:26 +0300

    Fix message when nothing found in cone search
    Fix #21

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ import dash_dangerously_set_inner_html as ddsih
 from astropy.table import Table
 
 from cross import find_ztf_circle
-from util import html_from_astropy_table
+from util import html_from_astropy_table, NotFound
 
 
 COLUMNS = {
@@ -21,14 +21,19 @@ COLUMNS = {
 def get_layout(coordinates, radius_arcsec, dr):
     ra = coordinates.ra.to_value('deg')
     dec = coordinates.dec.to_value('deg')
-    j = find_ztf_circle.find(ra, dec, radius_arcsec, dr)
-    if not j:
-        return html.Div('404')
+    cone_str = f'({ra:.5f} deg, {dec:.5f} deg), r = {radius_arcsec:.1f}″'
+    try:
+        j = find_ztf_circle.find(ra, dec, radius_arcsec, dr)
+    except NotFound:
+        return html.Div([
+            html.H1('404'),
+            f'Nothing inside cone {cone_str}',
+        ])
     table = Table([dict(oid=f'<a href="/{dr}/view/{oid}">{oid}</a>', separation=obj['separation'], **obj['meta'])
                    for oid, obj in sorted(j.items(), key=lambda x: x[1]['separation'])])
     layout = html.Div(
         [
-            html.H1(f'Objects inside cone ({ra:.5f} deg, {dec:.5f} deg), r = {radius_arcsec:.1f}″'),
+            html.H1(f'Objects inside cone {cone_str}'),
             ddsih.DangerouslySetInnerHTML(html_from_astropy_table(table, COLUMNS)),
         ],
     )
