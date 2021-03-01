37188bf2c7afbf71523992a695077f46201ae4f8
Konstantin Malanchev <hombit@gmail.com>
2021-03-01 12:46:10 +0300

    Update TNS API url
    Fix #36

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ REDIS_HOSTNAME = os.environ.get('REDIS_URL', 'redis')
 AKB_API_URL = os.environ.get('AKB_API_URL', 'https://akb.ztf.snad.space/')
 LC_API_URL = os.environ.get('LC_API_URL', 'http://db.ztf.snad.space')
 PRODUCTS_URL = os.environ.get('PRODUCTS_URL', 'http://ztf-web-viewer-proxy')
-TNS_API_URL = os.environ.get('TNS_API_URL', 'https://wis-tns.weizmann.ac.il')
+TNS_API_URL = os.environ.get('TNS_API_URL', 'https://www.wis-tns.org')
 
 TNS_API_KEY = os.environ.get('TNS_API_KEY', None)
