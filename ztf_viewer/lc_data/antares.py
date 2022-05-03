2568adb7c9e029c119d30aab48ff5e412a67df16
Konstantin Malanchev <hombit@gmail.com>
2022-05-03 07:22:03 -0500

    Fix removing non-detections from Antares

--- ztf_viewer/lc_data/antares.py
+++ ztf_viewer/lc_data/antares.py
@@ -1,4 +1,4 @@
-import math as m
+import numpy as np
 
 from ztf_viewer.catalogs import find_ztf_oid
 from ztf_viewer.catalogs.conesearch.antares import AntaresQuery
@@ -7,7 +7,8 @@ from ztf_viewer.catalogs.conesearch.antares import AntaresQuery
 def get_antares_lc(oid, dr, radius):
     coord = find_ztf_oid.get_sky_coord(oid, dr)
     _, locus = AntaresQuery.query_region_closest_locus(coord, radius)
-    lc = [
+    lc = locus.lightcurve[~np.isnan(locus.lightcurve['ant_mag'])]
+    return [
         {
             'oid': locus.locus_id,
             'mjd': obs.ant_mjd,
@@ -15,6 +16,5 @@ def get_antares_lc(oid, dr, radius):
             'magerr': obs.ant_magerr,
             'filter': f"ant_{obs.ant_passband}",
         }
-        for obs in locus.lightcurve.itertuples() if not m.isnan(obs['ant_mag'])
+        for obs in lc.itertuples()
     ]
-    return lc
