c23b47f265bb5fc23e57e83be285ca4fa210fef1
Konstantin Malanchev <hombit@gmail.com>
2022-05-17 18:04:14 -0500

    Pan-STARRS stacked apaerture mags

--- ztf_viewer/catalogs/conesearch/__init__.py
+++ ztf_viewer/catalogs/conesearch/__init__.py
@@ -7,6 +7,7 @@ from .gaia2dis import Gaia2Dis
 from .gaia_edr3_dis import GaiaEdr3Dis
 from .gcvs import GcvsQuery
 from .ogle import OgleQuery
+from .panstarrs import PanstarrsDr2StackedQuery
 from .sdss import SdssQuasarsQuery
 from .simbad import SimbadQuery
 from .tns import TnsQuery
@@ -22,6 +23,7 @@ GAIA2_DIS = Gaia2Dis('Gaia DR2 Distances')
 GAIA_EDR3_DIS = GaiaEdr3Dis('Gaia EDR3 Distances')
 GCVS_QUERY = GcvsQuery('GCVS')
 OGLE_QUERY = OgleQuery('OGLE')
+PANSTARRS_DR2_QUERY = PanstarrsDr2StackedQuery('Pan-STARRS DR2 Stacked')
 SDSS_QUASARS_QUERY = SdssQuasarsQuery('SDSS DR16 Quasars')
 SIMBAD_QUERY = SimbadQuery('Simbad')
 TNS_QUERY = TnsQuery('Transient Name Server')
