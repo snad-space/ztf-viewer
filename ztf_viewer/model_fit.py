efd50eac045e727c02ae28704d7a7e8392cdf57e
GaneevRizvan <104025596+GaneevRizvan@users.noreply.github.com>
2025-12-10 02:47:56 +0300

    Unavailability of API

--- ztf_viewer/model_fit.py
+++ ztf_viewer/model_fit.py
@@ -7,44 +7,25 @@ from ztf_viewer.catalogs.ztf_ref import ztf_ref
 from ztf_viewer.exceptions import NotFound, CatalogUnavailable
 from ztf_viewer.util import ABZPMAG_JY, LN10_04
 
-
 def post_request(url, data):
     try:
         response = requests.post(url, json=data.model_dump())
         response.raise_for_status()
         return response.status_code, response.json()
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")
-        return e.response.status_code, None
-    except requests.exceptions.ConnectionError as e:
-        print(f"Connection error: {e}")
-        return 0, None
-    except requests.exceptions.Timeout as e:
-        print(f"Timeout error: {e}")
-        return -1, None
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return -2, None
-
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError,
+            requests.exceptions.Timeout, requests.exceptions.RequestException) as e:
+        print(f"A model-fit-api error occurred: {e}")
+        return -1, {"error": "API is unavailable"}
 
 def get_request(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.status_code, response.json()
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")
-        return e.response.status_code, None
-    except requests.exceptions.ConnectionError as e:
-        print(f"Connection error: {e}")
-        return 0, None
-    except requests.exceptions.Timeout as e:
-        print(f"Timeout error: {e}")
-        return -1, None
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return -2, None
-
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError,
+            requests.exceptions.Timeout, requests.exceptions.RequestException) as e:
+        print(f"A model-fit-api error occurred: {e}")
+        return -1,{"error": "API is unavailable"}
 
 class Observation(BaseModel):
     mjd: float
@@ -54,13 +35,11 @@ class Observation(BaseModel):
     zp: float = ABZPMAG_JY
     zpsys: Literal["ab", "vega"] = "ab"
 
-
 class Target(BaseModel):
     light_curve: List[Observation]
     ebv: float
     name_model: str
     redshift: List[float] = [0.05, 0.3]
-
 
 class ModelData(BaseModel):
     parameters: Dict[str, float]
@@ -73,7 +52,6 @@ class ModelData(BaseModel):
     count: int = 2000
     brightness_type: str
     band_ref: Dict[str, float]
-
 
 class ModelFit:
     base_url = "https://fit.lc.snad.space/api/v1"
@@ -90,7 +68,7 @@ class ModelFit:
     def fit(self, df, fit_model, dr, ebv):
         self.set_path("/sncosmo/fit")
         df = df.copy()
-        if "ref_flux" not in df.columns:
+        if 'ref_flux' not in df.columns:
             oid_ref = {}
             try:
                 for objectid in df["oid"].unique():
@@ -105,8 +83,8 @@ class ModelFit:
                     for fluxerr, ref_flux, oid in zip(df["fluxerr_Jy"], df["ref_flux"], df["oid"])
                 ]
             except (NotFound, CatalogUnavailable):
-                print("Catalog error")
-                return {}
+                print(f"Catalog error")
+                return {'error': 'Catalog is unavailable'}
         status_code, res_fit = post_request(
             self.base_url + self.path,
             Target(
@@ -128,16 +106,18 @@ class ModelFit:
         if status_code == 200:
             return res_fit["parameters"]
         else:
-            return {}
+            return res_fit
 
     def get_curve(self, df, dr, bright, params, name_model):
         self.set_path("/sncosmo/get_curve")
+        if 'error' in params.keys():
+            return pd.DataFrame.from_records([])
         band_ref = {}
         band_list = ["ztf" + str(band[1:]) for band in df["filter"].unique()]
         mjd_min = df["mjd"].min()
         mjd_max = df["mjd"].max()
         df = df.copy()
-        if "ref_flux" not in df.columns:
+        if 'ref_flux' not in df.columns:
             oid_ref = {}
             try:
                 for objectid in df["oid"].unique():
@@ -146,12 +126,12 @@ class ModelFit:
                     oid_ref[objectid] = ref_mag
                 df["ref_flux"] = df["oid"].apply(lambda x: 10 ** (-0.4 * (oid_ref[x] - ABZPMAG_JY)))
             except (NotFound, CatalogUnavailable):
-                print("Catalog error")
+                print(f"Catalog error")
                 return pd.DataFrame.from_records([])
 
         for band in df["filter"].unique():
             band_ref[band] = df[df["filter"] == band]["ref_flux"].mean().astype(float)
-        status_code, res_fit = post_request(
+        status_code, res_curve = post_request(
             self.base_url + self.path,
             ModelData(
                 parameters=params,
@@ -164,7 +144,7 @@ class ModelFit:
             ),
         )
         if status_code == 200:
-            df_fit = pd.DataFrame.from_records(res_fit["bright"])
+            df_fit = pd.DataFrame.from_records(res_curve["bright"])
             df_fit["time"] = df_fit["time"] - 58000
             return df_fit
         else:
@@ -175,8 +155,7 @@ class ModelFit:
         status_code, list_models = get_request(self.base_url + self.path)
         if status_code == 200:
             return list_models["models"]
-        else:
-            return []
+        else: return []
 
 
 model_fit = ModelFit()
